061635b20e3873d3e46024e1ce37c8b168727d92
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-17 17:23:32 +0100

    modified API used for POSTing a Question

--- routes/question.js
+++ routes/question.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const questionController = require('../controllers/questions');
+const questionController = require('../controllers/question');
 
 router.post('/', questionController.postQuestion);
 
