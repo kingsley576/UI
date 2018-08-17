233faff8eb778acc4658fe99a5e5e944b3a0576f
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-17 17:17:22 +0100

    setup an API endpoint to get a question

--- routes/answer.js
+++ routes/answer.js
@@ -6,6 +6,5 @@ const indexController = require('../controllers/answer');
 
 router.get('/:questionId', indexController.GetOneQuestion);
 
-router.post('/', indexController.PostAnAnswer);
 
 module.exports = router;
