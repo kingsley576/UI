1c555b873a9559a028dbba07e520573a3a1fe61a
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-17 16:55:01 +0100

    modified Get-All-Questions Api

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 var qs = require('../questions.json');
 
-exports.indexGetAll = (req, res) => {
+exports.indexGetAll = (req, res, next) => {
   res.status(200).json({
     questions: qs.categories
   });
