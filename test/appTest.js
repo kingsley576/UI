5576d84a8b86948e2c36fd898f478d115bded983
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-19 22:02:39 +0100

    modified package.json

--- test/appTest.js
+++ test/appTest.js
@@ -2,12 +2,12 @@ describe("index", function() {
 
 
 	qs = {res: jasmine};
-	res = {json: {json: jasmine, status: jasmine}};
+	res = {json: jasmine, status: 200};
 	controller = require('./../controllers/index')(qs);
 
 	describe("indexGetAll", function() {
 
-		it('should call res', function() {
+		it('should call res.status.json', function() {
 			controller.indexGetAll(res);
 			expect(res).toHaveBeenCalled();
 		});
