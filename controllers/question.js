5f1999184a8302a5a24980c17fb9c03e1c9db208
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-19 19:47:11 +0100

    made controller test ready

--- controllers/question.js
+++ controllers/question.js
@@ -1,14 +1,21 @@
-exports.postQuestion = (req, res, next) => {
-  const qstn = {
-    username: req.body.username,
-    gender: req.body.gender,
-    id: req.body.id,
-    title: req.body.title,
-    question: req.body.question
+qsController = function(qs) {
+    const postQuestion = (req, res, next) => {
+      const qstn = {
+        username: req.body.username,
+        gender: req.body.gender,
+        id: req.body.id,
+        title: req.body.title,
+        question: req.body.question
 
-  };
-  res.status(201).json({
-    message: 'POSTED A QUESTION',
-    question: qstn
-  });
-};
+      };
+      res.status(201).json({
+        message: 'POSTED A QUESTION',
+        question: qstn
+      });
+    };
+  return {
+      postQuestion: postQuestion
+  }
+}
+
+module.exports = qsController;
