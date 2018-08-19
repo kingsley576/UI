1e61e931f3cc162e9e654c6787fbfcbc40d7c370
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-19 19:49:37 +0100

    made controller test ready

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const qs = require('../questions.json');
 
-const indexController = require('../controllers/index');
+const indexController = require('../controllers/index')(qs);
 
 router.get('/', indexController.indexGetAll);
 
