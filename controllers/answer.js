80d46f84d066a48ecb59b8a8ad4df060e3ceeac9
Kingsley576 <Obiohakingsley1@gmail.com>
2018-08-20 12:42:19 +0100

    made slight modifications to follow linting style guidelines

--- controllers/answer.js
+++ controllers/answer.js
@@ -1,27 +1,27 @@
-var qs = require('../questions.json');
+const qs = require('../questions.json');
 
 exports.GetOneQuestion = (req, res, next) => {
   var id = req.params.questionId;
   for (var i = 0; i < qs.categories.length; i++) {
     if (id === qs.categories[i].id) {
-    return res.status(200).json({
-      question: qs.categories[i]
-    });
+      return res.status(200).json({
+        question: qs.categories[i]
+      });
+    }
   }
-}
-}
+};
 
 exports.PostAnAnswer = (req, res, next) => {
   const ansr = {
-      username: req.body.username,
-      gender: req.body.gender,
-      id: req.body.id,
-      answer: req.body.answer
+    username: req.body.username,
+    gender: req.body.gender,
+    id: req.body.id,
+    answer: req.body.answer
 
   };
   res.status(201).json({
-    message: "POSTED AN ANSWER",
+    message: 'POSTED AN ANSWER',
     question: qs.categories[2].question,
     answer: ansr.answer
   });
-}
+};
--- test/appTest.js
+++ test/appTest.js
@@ -8,77 +8,73 @@ chai.use(chaiHttp);
 
 describe('indexGetAll', () => {
   it('should list ALL questions on /index/v1 GET', (done) => {
-  chai.request(app)
-    .get('/index/v1')
-    .end((err, res) => {
-      res.should.have.status(200);
-      done();
-    });
-});
+    chai.request(app)
+      .get('/index/v1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
 
 describe('postQuestion', () => {
   it('should POST questions on /question/v1 POST', (done) => {
-
   	const qstn = {
-      username: "kingsley",
-      gender: "male",
+      username: 'kingsley',
+      gender: 'male',
       id: 8,
-      title: "cars",
-      question: "fastest cars"
+      title: 'cars',
+      question: 'fastest cars'
 
-  }
+    };
 
-  chai.request(app)
-    .post('/question/v1')
+    chai.request(app)
+      .post('/question/v1')
     .send(qstn)
-    .end((err, res) => {
+      .end((err, res) => {
 	      res.should.have.status(201);
-      done();
-    });
-});
+        done();
+      });
+  });
 });
 
 describe('PostAnAnswer', () => {
-  it('should POST an answer to a question', (done) => {
-	  
+  it('should POST an answer to a question', (done) => {	  
 	  const ansr = {
-	      username: "Michael",
-	      gender: "Male",
+	      username: 'Michael',
+	      gender: 'Male',
 	      id: 9,
-	      answer: "Bugatti"
+	      answer: 'Bugatti'
 
 	  };
-  chai.request(app)
-    .post('/answer')
-    .send(ansr)
-    .end((err, res) => {
-      res.should.have.status(201);
-      res.body.should.have.property('question');
-      res.body.should.have.property('answer');
-      done();
-    });
- });
+    chai.request(app)
+      .post('/answer')
+      .send(ansr)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.have.property('question');
+        res.body.should.have.property('answer');
+        done();
+      });
+  });
 });
 
 describe('GetOneQuestion', () => {
-
-	it('should GET one question', (done) => {
-
+  it('should GET one question', (done) => {
 	  	const qstn = { 
-	  		username: "Ijeoma",
-		    gender: "Female",
-		    id: "2",
-		    title: "Difference between SQL and NOSQL",
-		    question: "Please can someone explain the difference between SQL and NOSQL for me."
+	  		username: 'Ijeoma',
+		    gender: 'Female',
+		    id: '2',
+		    title: 'Difference between SQL and NOSQL',
+		    question: 'Please can someone explain the difference between SQL and NOSQL for me.'
 	  	};
   		chai.request(app)
     	.get('/answer/' + qstn.id)
     	.send(qstn)
     	.end((err, res) => {
       	res.should.have.status(200);
-      done();
+        done();
     	});
-      });
+  });
   	});
 
